72b3dca1fcc3e757a3c0b9cf0ad006aaf87f96b7
j0303p <j0303p@gmail.com>
2024-11-07 11:26:54 +0900

    refactor: 서비스 분리

--- core-bank/src/main/java/com/example/core_bank/core_bank/authentication/controller/AuthenticationController.java
+++ core-bank/src/main/java/com/example/core_bank/core_bank/authentication/controller/AuthenticationController.java
@@ -2,6 +2,8 @@ package com.example.core_bank.core_bank.authentication.controller;
 
 import com.example.core_bank.core_bank.authentication.dto.ReqAuthentication;
 import com.example.core_bank.core_bank.authentication.service.AuthenticationService;
+import com.example.core_bank.core_bank.authentication.service.EmailService;
+import com.example.core_bank.core_bank.authentication.service.VerificationNumberStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/authentication")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private final VerificationNumberStorage verificationNumberStorage;
+    private final EmailService emailService;
 
     @PostMapping("/check/pinNumber")
     public ResponseEntity<?> checkEmailPinNumber(@RequestBody ReqAuthentication reqAuthentication) {
@@ -24,15 +28,17 @@ public class AuthenticationController {
         if (!result)
             return ResponseEntity.badRequest().build();
 
-        String key = authenticationService.sendEmailPinNumber(reqAuthentication.getEmail());
-        authenticationService.saveNumber(reqAuthentication.getEmail(), key);
+        String verificationNumber = emailService.sendVerificationEmail(reqAuthentication.getEmail());
+        verificationNumberStorage.saveNumber(reqAuthentication.getEmail(), verificationNumber);
+
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/check/emailNumber")
     public ResponseEntity<?> checkEmailNumber(@RequestBody ReqAuthentication reqAuthentication) {
         log.info("Check email pin number: " + reqAuthentication.getEmail());
-        boolean result = authenticationService.checkEmailNumber(reqAuthentication);
+        boolean result = verificationNumberStorage.verifyNumber(reqAuthentication);
+
         if (!result)
             return ResponseEntity.badRequest().build();
 
