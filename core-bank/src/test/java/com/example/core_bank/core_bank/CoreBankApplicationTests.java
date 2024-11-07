23e0ed882d730487887fafdacc13f86b461ab788
j0303p <j0303p@gmail.com>
2024-11-07 10:54:56 +0900

    refactor: 테스트 제거

--- core-bank/src/test/java/com/example/core_bank/core_bank/CoreBankApplicationTests.java
+++ core-bank/src/test/java/com/example/core_bank/core_bank/CoreBankApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.core_bank.core_bank;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CoreBankApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class CoreBankApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
