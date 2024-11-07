20a6866777d99d2e896c2d36f03ed0b10a164bb7
jihyuk0414 <dealon2580@naver.com>
2024-11-07 11:53:23 +0900

    hotfix#changeerdformat

--- Attendance/src/main/java/com/example/Attendance/model/Working.java
+++ Attendance/src/main/java/com/example/Attendance/model/Working.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "working")
@@ -19,13 +20,13 @@ public class Working {
     private LocalDate workDay;
 
     @Column(name = "work_duration")
-    private LocalDate workDuration;
+    private LocalTime workDuration;
 
     @Column(name = "start_time")
-    private LocalDate startTime;
+    private LocalTime startTime;
 
     @Column(name = "end_time")
-    private LocalDate endTime;
+    private LocalTime endTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "employee_id")
