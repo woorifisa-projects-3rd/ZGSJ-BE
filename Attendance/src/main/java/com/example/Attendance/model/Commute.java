354bcc9d2b51b7ffd1e6bec0614eb8a1bbf0aa3a
jihyuk0414 <dealon2580@naver.com>
2024-11-11 17:32:47 +0900

    fix:start, end time LocalDateTime으로 변경

--- Attendance/src/main/java/com/example/Attendance/model/Commute.java
+++ Attendance/src/main/java/com/example/Attendance/model/Commute.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import java.time.temporal.ChronoUnit;
@@ -26,16 +27,16 @@ public class Commute {
     private LocalTime commuteDuration;
 
     @Column(name = "start_time")
-    private LocalTime startTime;
+    private LocalDateTime startTime;
 
     @Column(name = "end_time")
-    private LocalTime endTime;
+    private LocalDateTime endTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "se_id")
     private StoreEmployee storeEmployee;
 
-    private Commute(LocalDate commuteDate, LocalTime startTime, StoreEmployee storeEmployee) {
+    private Commute(LocalDate commuteDate, LocalDateTime startTime, StoreEmployee storeEmployee) {
         this.commuteDate = commuteDate;
         this.commuteDuration = LocalTime.of(0, 0);
         this.startTime = startTime;
@@ -43,7 +44,7 @@ public class Commute {
     }
 
     //사장님이 출퇴근 모두 지정 용
-    private Commute(LocalDate commuteDate, LocalTime startTime, LocalTime endTime, StoreEmployee storeEmployee) {
+    private Commute(LocalDate commuteDate, LocalDateTime startTime, LocalDateTime endTime, StoreEmployee storeEmployee) {
         this.commuteDate = commuteDate;
         this.startTime = startTime;
         this.endTime = endTime;
@@ -52,15 +53,15 @@ public class Commute {
     }
 
 
-    public static Commute createCommuteCheckIn(LocalDate commuteDate, LocalTime startTime, StoreEmployee storeEmployee) {
+    public static Commute createCommuteCheckIn(LocalDate commuteDate, LocalDateTime startTime, StoreEmployee storeEmployee) {
         return new Commute(commuteDate, startTime, storeEmployee);
     }
 
-    public static Commute createCompleteCommute(LocalDate commuteDate, LocalTime startTime, LocalTime endTime, StoreEmployee storeEmployee) {
+    public static Commute createCompleteCommute(LocalDate commuteDate, LocalDateTime startTime, LocalDateTime endTime, StoreEmployee storeEmployee) {
         return new Commute(commuteDate, startTime, endTime, storeEmployee);
     }
 
-    private LocalTime calculateDuration(LocalTime startTime, LocalTime endTime) {
+    private LocalTime calculateDuration(LocalDateTime startTime, LocalDateTime endTime) {
         long hours = ChronoUnit.HOURS.between(startTime, endTime);
         long minutes = ChronoUnit.MINUTES.between(startTime, endTime) % 60;
         return LocalTime.of((int) hours, (int) minutes);
