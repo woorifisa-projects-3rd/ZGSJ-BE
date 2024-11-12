0d3082f78b346dbdaf0005faaa6b1d9a668a9bde
jihyuk0414 <dealon2580@naver.com>
2024-11-12 10:24:26 +0900

    feat: 손익계산서 PDF 작업

---
+++ User/src/main/java/com/example/User/service/CoreBankService.java
@@ -0,0 +1,11 @@
+package com.example.User.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CoreBankService {
+
+    private final
+}
