fba265fbe82b71f58418c64e463b42ec0ac6cb18
j0303p <j0303p@gmail.com>
2024-11-12 12:21:14 +0900

    feat: qr 출퇴근 로직 및 key제거

--- Attendance/src/main/java/com/example/Attendance/error/ErrorCode.java
+++ Attendance/src/main/java/com/example/Attendance/error/ErrorCode.java
@@ -23,7 +23,7 @@ public enum ErrorCode {
     SERVER_ERROR(500, "서버 에러입니다. 서버 팀에 연락주세요!"),
     CANNOT_FIND_POSITION(400,"위치가 잘못됨"),
     INVALID_EMPLOYEE(400,"직원 정보가 없음"),
-    INVALID_LOCATION(400,"직원 정보가 없음"),
+    INVALID_LOCATION(403,"위치가 잘못됨"),
     INVALID_COMMUTE(400,"통근 정보가 없음"),
     MISSING_GO_TO_WORK_RECODE(400,"출근 찍지 않음"),
     MISSING_LEAVE_WORK_RECODE(400,"퇴근 찍지 않음");
