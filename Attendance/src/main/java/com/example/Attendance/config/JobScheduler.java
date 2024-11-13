5a408a8b926206a38c977836be171e80372ab812
Hyunah Lee <hyuna030619@naver.com>
2024-11-13 17:41:47 +0900

    feat: 불필요한 로직 삭제

--- Attendance/src/main/java/com/example/Attendance/config/JobScheduler.java
+++ Attendance/src/main/java/com/example/Attendance/config/JobScheduler.java
@@ -62,28 +62,3 @@ public class JobScheduler {
         }
     }
 }
-
-
-//@Slf4j
-//@Configuration
-//@EnableScheduling  // 스케줄링 기능 활성화
-//@RequiredArgsConstructor
-//public class JobScheduler {
-//
-//    private final JobLauncher jobLauncher;
-//    private final Job attendanceJob;  // AttendanceJobConfig에서 정의한 Job
-//
-//    @Scheduled(cron = "0 0 4 * * *")  // 매일 새벽 4시 실행
-//    public void runJob() {
-//        try {
-//            JobParameters jobParameters = new JobParametersBuilder()
-//                    .addLong("time", System.currentTimeMillis())
-//                    .toJobParameters();
-//
-//            JobExecution execution = jobLauncher.run(attendanceJob, jobParameters);
-//            log.info("Job Status : {}", execution.getStatus());
-//        } catch (Exception e) {
-//            log.error("Job Failed : {}", e.getMessage());
-//        }
-//    }
-//}
