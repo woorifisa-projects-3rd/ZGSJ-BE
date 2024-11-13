2f4910e26bb76244ee7b833bb288be7384eed04d
j0303p <j0303p@gmail.com>
2024-11-13 18:59:11 +0900

    refactor: attendance 서비스 분리 및 공백 제거

--- Attendance/src/main/java/com/example/Attendance/Repository/CommuteRepository.java
+++ Attendance/src/main/java/com/example/Attendance/Repository/CommuteRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CommuteRepository extends JpaRepository<Commute, Integer> {
-
-
     Optional<Commute> findTopByStoreEmployeeIdOrderByStartTimeDesc(Integer seId);
 }
