fd93b152c7912e0d068636dad7a12e4432cadb40
j0303p <j0303p@gmail.com>
2024-11-13 15:37:18 +0900

    feat: 코어 automaticTransfer api 생성

---
+++ core-bank/src/main/java/com/example/core_bank/core_bank/authentication/dto/TransferRequest.java
@@ -0,0 +1,22 @@
+package com.example.core_bank.core_bank.authentication.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferRequest {
+    private String fromAccount;
+    private String fromBankCode;
+    private String toAccount;
+    private String toBankCode;
+    private Long amount;
+    private String toAccountDepositor;
+    private String fromAccountDepositor;
+
+}
