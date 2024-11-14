8d8d36c1c33a5400aaec181ffcb8670379853b23
jihyuk0414 <dealon2580@naver.com>
2024-11-14 15:54:00 +0900

    fix# 생성자 추가

--- Finance/src/main/java/com/example/Finance/dto/AccountInfoResponse.java
+++ Finance/src/main/java/com/example/Finance/dto/AccountInfoResponse.java
@@ -1,8 +1,10 @@
 package com.example.Finance.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class AccountInfoResponse {
     String accountNumber ;
     String bankCode;
