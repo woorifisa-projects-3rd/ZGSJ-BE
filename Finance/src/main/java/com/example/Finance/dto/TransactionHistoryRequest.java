5d2468735911633467c48d1766171f341e2f43dd
jihyuk0414 <dealon2580@naver.com>
2024-11-14 15:54:08 +0900

    fix# 어노테이션 제거

--- Finance/src/main/java/com/example/Finance/dto/TransactionHistoryRequest.java
+++ Finance/src/main/java/com/example/Finance/dto/TransactionHistoryRequest.java
@@ -9,7 +9,6 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@ToString
 public class TransactionHistoryRequest {
     private String account;
     private String bankCode;
