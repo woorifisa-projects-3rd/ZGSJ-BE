f1996de57ec7d19ac574f1f4b249816a0a6ad45c
j0303p <j0303p@gmail.com>
2024-11-14 20:51:59 +0900

    refactor: 가시성 및 null 고려 및 valid 는 util에서

--- API-Gateway/src/main/java/com/example/API_Gateway/filter/TokenCheckFilter.java
+++ API-Gateway/src/main/java/com/example/API_Gateway/filter/TokenCheckFilter.java
@@ -4,21 +4,18 @@ package com.example.API_Gateway.filter;
 import com.example.API_Gateway.error.CustomException;
 import com.example.API_Gateway.error.ErrorCode;
 import com.example.API_Gateway.util.JWTUtil;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 @Slf4j
@@ -51,15 +48,12 @@ public class TokenCheckFilter implements GlobalFilter, Ordered {
             return chain.filter(exchange);
         }
 
-        if (!headers.containsKey(HttpHeaders.AUTHORIZATION)) {
-            log.info("헤더에 정보가 없습니다");
-            return onError(exchange, HttpStatus.UNAUTHORIZED);
-        }
+        String tokenStr = Optional.ofNullable(headers.get(HttpHeaders.AUTHORIZATION))
+                .flatMap(authHeaders -> authHeaders.stream().findFirst())
+                .orElseThrow(() -> new CustomException(ErrorCode.UNAUTHORIZED))
+                .replace("Bearer ", "");
 
-        String tokenStr = headers.get(HttpHeaders.AUTHORIZATION)
-                .get(0).replace("Bearer ", "");
-
-        Map<String, Object> payload = validateAccessToken(tokenStr,path);
+        Map<String, Object> payload = jwtUtil.validateToken(tokenStr,path);
 
         if(!needIdUrl.stream().anyMatch(path::contains)) {
             log.info("통과");
@@ -75,27 +69,8 @@ public class TokenCheckFilter implements GlobalFilter, Ordered {
                 .build();
         return chain.filter(exchange.mutate().request(mutatedRequest).build());
     }
-
-    private Map<String, Object> validateAccessToken(String tokenStr,String path){
-        try {
-            return jwtUtil.validateToken(tokenStr);
-        } catch (MalformedJwtException malformedJwtException) {
-            log.error("MalformedJwtException----------------------");
-            throw new CustomException(ErrorCode.MALFORM_TOKEN);
-        } catch (SignatureException signatureException) {
-            log.error("SignatureException----------------------");
-            throw new CustomException(ErrorCode.BADSIGN_TOKEN);
-        } catch (ExpiredJwtException expiredJwtException) {
-            log.error("ExpiredJwtException----------------------");
-            if(path.contains("/president/refresh"))
-                return expiredJwtException.getClaims();
-            throw  new CustomException(ErrorCode.BAD_GATEWAY_TEST);
-//            throw new CustomException(ErrorCode.EXPIRED_TOKEN);
-        }
-    }
-
-    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
-        exchange.getResponse().setStatusCode(status);
-        return exchange.getResponse().setComplete();
-    }
+//    private Mono<Void> onError(ServerWebExchange exchange, HttpStatus status) {
+//        exchange.getResponse().setStatusCode(status);
+//        return exchange.getResponse().setComplete();
+//    }
 }
