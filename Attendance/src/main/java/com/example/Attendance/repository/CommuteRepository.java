ac27187ea2aac8104d12b7378b39d56efbf6d60c
j0303p <j0303p@gmail.com>
2024-11-15 15:48:15 +0900

    merge: R->r로 인한 충돌로 사라진 것 복구

--- Attendance/src/main/java/com/example/Attendance/repository/CommuteRepository.java
+++ Attendance/src/main/java/com/example/Attendance/repository/CommuteRepository.java
@@ -3,15 +3,36 @@ package com.example.Attendance.repository;
 import com.example.Attendance.dto.CommuteSummary;
 import com.example.Attendance.model.Commute;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface CommuteRepository extends JpaRepository<Commute, Integer> {
 
+    @Modifying
+    @Query("UPDATE Commute c " + "SET c.startTime = :startTime, " + "c.endTime = :endTime, " +
+            "c.commuteDate = :commuteDate, " + "c.commuteDuration = :commuteDuration " + "WHERE c.id = :commuteId")
+    void updateCommute(
+            @Param("startTime") LocalDateTime startTime, @Param("endTime") LocalDateTime endTime, @Param("commuteDate") LocalDate commuteDate,
+            @Param("commuteDuration") Long commuteDuration, @Param("commuteId") Integer commuteId);
+
+    @Query("SELECT c FROM Commute c " +
+            "JOIN FETCH c.storeEmployee se " +
+            "JOIN FETCH se.store s " +
+            "WHERE s.id = :storeId " +
+            "AND YEAR(c.commuteDate) = :year " +
+            "AND MONTH(c.commuteDate) = :month " +
+            "ORDER BY c.commuteDate, c.startTime")
+    List<Commute> findMonthlyCommutesByStore(
+            @Param("storeId") Integer storeId,
+            @Param("year") int year,
+            @Param("month") int month
+    );
 
     Optional<Commute> findTopByStoreEmployeeIdOrderByStartTimeDesc(Integer seId);
 
@@ -24,4 +45,5 @@ public interface CommuteRepository extends JpaRepository<Commute, Integer> {
             @Param("startDate")LocalDate startDate,
             @Param("endDate")LocalDate endDate,
             @Param("employeeIds") List<Integer> employeeIds);
+
 }
