4c2617858f7b98130adb9fab39b0edd0c531fbff
jihyuk0414 <dealon2580@naver.com>
2024-11-15 10:58:01 +0900

    feat: 통신 과정 오류 발생 테스트

--- Finance/src/test/java/com/example/Finance/service/TransactionHistoryTestMock.java
+++ Finance/src/test/java/com/example/Finance/service/TransactionHistoryTestMock.java
@@ -2,21 +2,28 @@ package com.example.Finance.service;
 
 import com.example.Finance.dto.TransactionHistoryRequest;
 import com.example.Finance.dto.TransactionHistoryResponse;
+import com.example.Finance.error.ErrorDTO;
+import com.example.Finance.error.GlobalExceptionHandler;
 import com.example.Finance.feign.CoreBankFeign;
 import feign.FeignException;
 import feign.Request;
 import feign.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -90,7 +97,36 @@ public class TransactionHistoryTestMock {
                 transactionHistoryService.getYearMonthlyTransactions(transactionHistoryRequest, 2024, 11))
                 .isInstanceOf(FeignException.class)
                 .hasMessageContaining("존재하지 않는 사용자의 계좌입니다");
+    }
+    
+    @Test
+    @DisplayName("feign통신 과정에서 오류 발생")
+    public void feignwrong()
+    {
+        TransactionHistoryRequest request = new TransactionHistoryRequest();
+        Integer year = 2023;
+        Integer month = 5;
+        String errorDetail = "Bad request";
 
+        Map<String, Collection<String>> headers = new HashMap<>();
+        headers.put("Content-Type", Collections.singletonList("application/json"));
 
+        Request request2 = Request.create(
+                Request.HttpMethod.GET,
+                "/transaction-history",
+                headers,
+                new byte[0],
+                StandardCharsets.UTF_8,
+                null
+        );
+
+        Mockito.when(coreBankFeign.getTransactionHistoryList(request, year, month))
+                .thenThrow(new FeignException.BadRequest(errorDetail, request2, new byte[0], Collections.emptyMap()));
+
+        FeignException exception = Assertions.assertThrows(FeignException.class, () -> {
+            transactionHistoryService.getYearMonthlyTransactions(request, year, month);
+        });
+
+        Assertions.assertEquals("Bad request", exception.getMessage());
     }
 }
