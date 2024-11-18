013d657344dc8a24d8564d941ab7a6347f1473ac
ksp0814 <rkdtpvlf456@naver.com>
2024-11-18 15:05:40 +0900

    fix: NewPassword 패턴 추가

--- User/src/main/java/com/example/User/dto/login/ReqPwChange.java
+++ User/src/main/java/com/example/User/dto/login/ReqPwChange.java
@@ -2,6 +2,7 @@ package com.example.User.dto.login;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,5 +16,7 @@ public class ReqPwChange {
     private String beforePassword;
 
     @NotBlank(message = "바꿀 비밀번호를 입력하십시오.")
+    @Pattern(regexp = "^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$",
+            message = "비밀번호는 최소 8자 이상이며, 특수문자(!@#$%^&*)를 1개 이상 포함해야 합니다.")
     private String newPassword;
 }
