ef0e9e5c04dae779a4d25a7decfc753e5758abd2
jihyuk0414 <dealon2580@naver.com>
2024-11-18 15:01:29 +0900

    feat : 급여 확인 쿼리 생성

--- Attendance/src/main/java/com/example/Attendance/repository/PayStatementRepository.java
+++ Attendance/src/main/java/com/example/Attendance/repository/PayStatementRepository.java
@@ -1,8 +1,32 @@
 package com.example.Attendance.repository;
 
+import com.example.Attendance.dto.PayStatementResponse;
 import com.example.Attendance.model.PayStatement;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PayStatementRepository extends CrudRepository<PayStatement, Integer> {
+
+    @Query("SELECT " +
+            "  new com.example.Attendance.dto.PayStatementResponse(" +
+            "    ps.id, " +
+            "    se.bankCode, " +
+            "    se.accountNumber, " +
+            "    ps.amount, " +
+            "    ps.issuanceDate" +
+            "  ) " +
+            "FROM PayStatement ps " +
+            "INNER JOIN StoreEmployee se ON ps.seId = se.id " +
+            "WHERE se.store.id = :storeId " +
+            "AND FUNCTION('YEAR', ps.issuanceDate) = :year " +
+            "AND FUNCTION('MONTH', ps.issuanceDate) = :month")
+    List<PayStatementResponse> findPayStatementResponsesByStoreAndDateWithFetch(
+            @Param("storeId") Integer storeId,
+            @Param("year") Integer year,
+            @Param("month") Integer month
+    );
 
 }
