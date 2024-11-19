c88c4e92fc32321fac52108728287affc255ae47
j0303p <j0303p@gmail.com>
2024-11-19 11:36:23 +0900

    feat: native 사용으로 insert 1번 및 동시에

--- core-bank/src/main/java/com/example/core_bank/core_bank/core/repository/TransactionHistoryRepository.java
+++ core-bank/src/main/java/com/example/core_bank/core_bank/core/repository/TransactionHistoryRepository.java
@@ -2,9 +2,11 @@ package com.example.core_bank.core_bank.core.repository;
 
 import com.example.core_bank.core_bank.core.model.TransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Integer> {
@@ -31,4 +33,20 @@ public interface TransactionHistoryRepository extends JpaRepository<TransactionH
             @Param("accountId") Integer accountId,
             @Param("year") Integer year
     );
+
+    // 1번의 쿼리로 2개의 데이터 삽입
+    @Modifying
+    @Query(value = "INSERT INTO transaction_history " +
+            "(transaction_date, amount, is_deposit, transaction_type, account_id, classfication_id) " +
+            "VALUES " +
+            "(:now, :amount, false, '이체', :fromAccountId, :fromClassificationId), " +
+            "(:now, :amount, true, '입금', :toAccountId, :toClassificationId)",
+            nativeQuery = true)
+    void saveTransactionHistories(
+            @Param("fromAccountId") Integer fromAccountId,
+            @Param("toAccountId") Integer toAccountId,
+            @Param("amount") Long amount,
+            @Param("fromClassificationId") Integer fromClassificationId,
+            @Param("toClassificationId") Integer toClassificationId,
+            @Param("now") LocalDate now);
 }
