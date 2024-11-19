df4bf722aad9d7f662b2abbd3c9dd56b6a1c36d5
j0303p <j0303p@gmail.com>
2024-11-19 10:05:34 +0900

    feat: account find 쿼리문 추가

--- core-bank/src/main/java/com/example/core_bank/core_bank/core/repository/AccountRepository.java
+++ core-bank/src/main/java/com/example/core_bank/core_bank/core/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.example.core_bank.core_bank.core.repository;
 import com.example.core_bank.core_bank.core.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +13,12 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     @Query("select a from Account a join fetch a.bank where a.accountNumber = :accountNumber")
     Optional<Account> findByAccountNumberWithBank(String accountNumber);
+
+    @Query("select a from Account a join fetch a.bank b " +
+            "where a.accountNumber = :accountNumber " +
+            "and b.bankCode= :bankCode and a.name= :name")
+    Optional<Account> findByAccountNumberAndBankCodeAndName(
+            @Param("accountNumber") String accountNumber,
+            @Param("bankCode") String bankCode,
+            @Param("name") String name);
 }
