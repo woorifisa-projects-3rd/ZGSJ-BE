3e3b0482547542dcbda2cd5a1dbf06868be1750a
j0303p <j0303p@gmail.com>
2024-11-20 18:59:59 +0900

    feat: batch state 생성

---
+++ Attendance/src/main/java/com/example/Attendance/config/BatchJobState.java
@@ -0,0 +1,46 @@
+package com.example.Attendance.config;
+
+import com.example.Attendance.dto.BatchInputData;
+import com.example.Attendance.dto.CommuteSummary;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@JobScope   // Job 스코프로 생성
+@Getter
+@Setter
+public class BatchJobState {
+    private List<BatchInputData> employees;
+    private int currentIndex = 0;
+    private LocalDate localDate;
+    private List<CommuteSummary> commutes;
+    private List<Integer> employeeIds;
+
+    public void setEmployees(List<BatchInputData> employees) {
+        this.employees = employees;
+        this.employeeIds = employees.stream()
+                .map(BatchInputData::getSeId)
+                .toList();
+    }
+
+    public void setLocalDate() {
+        this.localDate = LocalDate.now();
+    }
+
+    public void indexUp() {
+        this.currentIndex+=1;
+    }
+
+    public Long getCommuteDuration(Integer seId) {
+        return this.getCommutes().stream()
+                .filter(commuteSummary -> commuteSummary.getEmployeeId().equals(seId))
+                .map(CommuteSummary::getCommuteDuration)
+                .findFirst()
+                .orElse(0L);
+    }
+}
