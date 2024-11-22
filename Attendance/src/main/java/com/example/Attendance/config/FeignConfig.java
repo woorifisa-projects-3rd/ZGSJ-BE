6f6ee77ee51c8c4fec88f89e91892219fb171178
j0303p <j0303p@gmail.com>
2024-11-22 11:04:43 +0900

    feat: fegin용 에러처리 추가

---
+++ Attendance/src/main/java/com/example/Attendance/config/FeignConfig.java
@@ -0,0 +1,16 @@
+package com.example.Attendance.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Retryer retryer() {
+        // period: 초기 재시도 간격(ms)
+        // maxPeriod: 최대 재시도 간격(ms)
+        // maxAttempts: 최대 재시도 횟수
+        return new Retryer.Default(1000, 2000, 3);
+    }
+}
