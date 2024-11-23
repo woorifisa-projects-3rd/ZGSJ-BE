33c851d8b18ae7d4812e58ba9cd2c9238a129309
j0303p <j0303p@gmail.com>
2024-11-23 18:12:10 +0900

    feat: url 변경

--- User/src/main/java/com/example/User/controller/ValidationController.java
+++ User/src/main/java/com/example/User/controller/ValidationController.java
@@ -2,8 +2,6 @@ package com.example.User.controller;
 
 import com.example.User.dto.businessnumber.BusinessNumberRequest;
 import com.example.User.dto.businessnumber.BusinessNumberResponse;
-import com.example.User.error.CustomException;
-import com.example.User.error.ErrorCode;
 import com.example.User.service.ValidationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +15,10 @@ public class ValidationController {
 
     private final ValidationService validationService;
 
-    @PostMapping("/businesscheck")
-    public ResponseEntity<String> validateBusinessNumber(@RequestBody BusinessNumberRequest businessNumberRequest)
+    @PostMapping("/store/businesscheck")
+    public ResponseEntity<Boolean> validateBusinessNumber(@RequestBody BusinessNumberRequest businessNumberRequest)
     {
         BusinessNumberResponse response = validationService.validateBusinessNumber(businessNumberRequest);
-
-        //이 에러 처리 부분 위에 통합하는게 좋을 듯?
-        if (!response.isExists())
-            throw new CustomException(ErrorCode.BUSINESSNUMBER_NOT_FOUND);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(response.isExists());
     }
 }
