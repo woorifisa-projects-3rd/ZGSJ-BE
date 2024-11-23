3b27e3762d0d39c12c5d31189c99e9440ac16aad
j0303p <j0303p@gmail.com>
2024-11-23 18:13:14 +0900

    feat: 인증서버 프로필,pin,emailpin 처리 추가

---
+++ User/src/main/java/com/example/User/controller/AccountAuthController.java
@@ -0,0 +1,54 @@
+package com.example.User.controller;
+
+import com.example.User.dto.authserver.AuthServerEmailPinNumber;
+import com.example.User.dto.authserver.AuthServerPinNumber;
+import com.example.User.dto.corebank.AccountAndCodeRequest;
+import com.example.User.dto.authserver.AuthServerProfileRequest;
+import com.example.User.resolver.MasterId;
+import com.example.User.service.CoreBankService;
+import com.example.User.service.PresidentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/core/account")
+public class AccountAuthController {
+
+    private final CoreBankService coreBankService;
+    private final PresidentService presidentService;
+
+    @PostMapping("/check")
+    public ResponseEntity<Boolean> getAccountBankCodeAndAccountNumber(
+            @MasterId Integer id,
+            @RequestBody AccountAndCodeRequest accountAndCodeRequest) {
+        // 서비스 호출
+        boolean isValid = coreBankService.getNameByIdAndBankCodeAndAccountNumber(
+                id,
+                accountAndCodeRequest.getAccountNumber(),
+                accountAndCodeRequest.getBankCode()
+        );
+
+        // 프론트로 boolean 값 반환
+        return ResponseEntity.ok(isValid);
+    }
+
+    @PostMapping("/profile")
+    public  ResponseEntity<Boolean> findUserToAuthServer(@RequestBody AuthServerProfileRequest profileRequest){
+        boolean result= coreBankService.verifyProfile(profileRequest);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/email/pin")
+    public ResponseEntity<Boolean> checkAuthEmailPinNumber(@RequestBody AuthServerEmailPinNumber emailPinNumber){
+        boolean result= coreBankService.checkEmailPinNumber(emailPinNumber);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/pin")
+    public ResponseEntity<Boolean> checkAuthPinNumber(@RequestBody AuthServerPinNumber pinNumber){
+        boolean result= coreBankService.checkPinNumber(pinNumber);
+        return ResponseEntity.ok(result);
+    }
+}
--- User/src/main/java/com/example/User/feign/CoreBankFeign.java
+++ User/src/main/java/com/example/User/feign/CoreBankFeign.java
@@ -1,9 +1,11 @@
 package com.example.User.feign;
 
+import com.example.User.dto.authserver.AuthServerEmailPinNumber;
+import com.example.User.dto.authserver.AuthServerPinNumber;
 import com.example.User.dto.businessnumber.BusinessNumberResponse;
 import com.example.User.dto.corebank.AccountCheckRequest;
+import com.example.User.dto.authserver.AuthServerProfileRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,4 +19,13 @@ public interface CoreBankFeign {
 
     @PostMapping("/bank/verify-account")
     boolean verifyAccount(@RequestBody AccountCheckRequest accountCheckRequest);
+
+    @PostMapping("/authentication/profile/check")
+    boolean verifyProfile(@RequestBody AuthServerProfileRequest profileRequest);
+
+    @PostMapping("/authentication/email/pincheck")
+    boolean checkEmailPinNumber(@RequestBody AuthServerEmailPinNumber emailPinNumber);
+
+    @PostMapping("/authentication/pincheck")
+    boolean checkPinNumber(@RequestBody AuthServerPinNumber checkPinNumber);
 }
