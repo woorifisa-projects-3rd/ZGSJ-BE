cfe2a10026108b9d1cd6521f546c4bf46f60b658
jihyuk0414 <dealon2580@naver.com>
2024-11-25 16:19:14 +0900

    feat : 비밀번호검증로직 추가

--- User/src/main/java/com/example/User/service/AuthService.java
+++ User/src/main/java/com/example/User/service/AuthService.java
@@ -1,5 +1,10 @@
 package com.example.User.service;
 
+import com.example.User.config.UserConfig;
+import com.example.User.error.CustomException;
+import com.example.User.error.ErrorCode;
+import com.example.User.model.President;
+import com.example.User.repository.PresidentRepository;
 import com.example.User.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +18,8 @@ import java.time.Duration;
 public class AuthService {
     private final JWTUtil jwtUtil;
     private final RedisTokenService tokenService;
+    private final PresidentRepository presidentRepository;
+    private final UserConfig userConfig;
 
     public String onAuthenticationSuccess(Integer id) {
         log.info("id {}", id);
@@ -23,5 +30,18 @@ public class AuthService {
         tokenService.setValues(id, refreshToken, Duration.ofDays(1));
 
         return accessToken;
+    }
+
+    public boolean validatePassword(Integer id, String passwordclaim)
+    {
+        President president =  presidentRepository.findById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.PRESIDENT_NOT_FOUND));
+
+       if (!userConfig.passwordEncoder().matches(passwordclaim, president.getPassword()))
+       {
+           throw new CustomException(ErrorCode.PASSWORD_WRONG);
+       }
+
+       return true;
     }
 }
