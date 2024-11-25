43b20dc2a81b1ee9a0496f9e457bca8ebe20cf8d
j0303p <j0303p@gmail.com>
2024-11-25 15:33:42 +0900

    feat: pdf 생성 및 cloud 저장 batch 분리

---
+++ Attendance/src/main/java/com/example/Attendance/config/attendanceJob/step/pdf/PdfBatchState.java
@@ -0,0 +1,34 @@
+package com.example.Attendance.config.attendanceJob.step.pdf;
+
+import com.example.Attendance.config.attendanceJob.Batch;
+import com.example.Attendance.dto.batch.pdf.PdfInputData;
+import lombok.Getter;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+@Component
+@JobScope   // StepScope 대신 JobScope 사용
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)  // Job 스코프로 생성
+@Getter
+public class PdfBatchState {
+
+    private List<PdfInputData> batches;
+    private int index=0;
+
+    public void findAllByLocalDate(List<Batch> batches) {
+        this.batches =batches.stream().map(PdfInputData::from).toList();
+    }
+    public void upIndex(){
+        this.index++;
+    }
+    public void reset(){
+        this.index=0;
+        this.batches=null;
+    }
+}
---
+++ Attendance/src/main/java/com/example/Attendance/config/attendanceJob/step/pdf/PdfBatchWriter.java
@@ -0,0 +1,44 @@
+package com.example.Attendance.config.attendanceJob.step.pdf;
+
+import com.example.Attendance.config.attendanceJob.BatchService;
+import com.example.Attendance.dto.batch.BatchOutputData;
+import com.example.Attendance.dto.batch.pdf.PdfOutputData;
+import com.example.Attendance.dto.batch.pdf.PdfSaveData;
+import com.example.Attendance.model.PayStatement;
+import com.example.Attendance.repository.PayStatementRepository;
+import com.example.Attendance.service.EmailService;
+import com.example.Attendance.service.GCPService;
+import com.example.Attendance.service.PayStatementPdfService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class PdfBatchWriter {
+    private final PayStatementRepository payStatementRepository;
+    private final BatchService batchService;
+
+    @Bean
+    @StepScope
+    public ItemWriter<PdfOutputData> pdfWriter() {
+        return chunk -> {
+            List<PayStatement> payStatements = chunk.getItems().stream()
+                    .map(PdfOutputData::toEntity).toList();
+
+            List<PdfSaveData> dataList= chunk.getItems().stream()
+                    .map(PdfOutputData::toPdfSaveData).toList();
+
+            batchService.updatePdfResultsAndUrls(dataList);
+            payStatementRepository.saveAll(payStatements);
+            log.info("급여 이체 결과 {} 건 저장 완료", chunk.size());
+        };
+    }
+}
