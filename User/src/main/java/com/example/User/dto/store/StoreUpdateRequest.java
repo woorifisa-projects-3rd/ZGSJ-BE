a5dd6cec4549686b273854fc354528fa500d2a57
j0303p <j0303p@gmail.com>
2024-11-29 18:35:16 +0900

    hotfix: user StoreUpdateRequest 추가

---
+++ User/src/main/java/com/example/User/dto/store/StoreUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.example.User.dto.store;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreUpdateRequest {
+    @NotBlank
+    private String storeName;
+    @NotBlank
+    private String accountNumber;
+    @NotBlank
+    private String location;
+    @NotNull
+    private Double latitude;
+    @NotNull
+    private Double longitude;
+}
