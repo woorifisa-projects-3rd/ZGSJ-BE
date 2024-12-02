d1a18e6c0d028a9af5e3c12ebdc298980618146c
jihyuk0414 <dealon2580@naver.com>
2024-12-02 11:10:31 +0900

    fix : core bank feign port 변경

--- Attendance/src/main/java/com/example/Attendance/FeignWithCoreBank.java
+++ Attendance/src/main/java/com/example/Attendance/FeignWithCoreBank.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Component
-@FeignClient(name = "core",url= "http://3.39.182.226:3456")
+@FeignClient(name = "core",url= "http://3.39.182.226:3030/bank")
 public interface FeignWithCoreBank {
 
     @PostMapping("/automaticTransfer")
