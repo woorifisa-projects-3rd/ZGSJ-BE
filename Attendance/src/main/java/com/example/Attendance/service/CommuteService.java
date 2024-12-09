3ca74b29e83ea38f5dc56036413b4a02c86c9e31
jihyuk0414 <dealon2580@naver.com>
2024-12-09 18:48:41 +0900

    fix : commuteDate 날짜 변경 로직 수정

--- Attendance/src/main/java/com/example/Attendance/service/CommuteService.java
+++ Attendance/src/main/java/com/example/Attendance/service/CommuteService.java
@@ -34,9 +34,10 @@ public class CommuteService {
     public boolean goToWork(StoreEmployee storeEmployee){
         Optional<Commute> lastCommute= commuteRepository.findTopByStoreEmployeeIdOrderByStartTimeDesc(storeEmployee.getId());
         LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        LocalDate commuteDate = now.toLocalDate();
 
         log.info("현재 시간은 "+ now);
-        Commute commute= Commute.createCommuteCheckIn(now.toLocalDate(),now,storeEmployee);
+        Commute commute= Commute.createCommuteCheckIn(commuteDate,now,storeEmployee);
         if(lastCommute.isPresent() &&lastCommute.get().getEndTime()==null){
             commuteRepository.save(commute);
             return false;
